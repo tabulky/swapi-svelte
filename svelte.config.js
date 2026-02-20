dd4c759480169ee6ebe3abd2c1668cbcc0ec616b
Pavel Lang <langpavel@phpskelet.org>
2026-02-20 15:32:40 +0100

    chore: enable Async Svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,25 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  kit: {
+    adapter: adapter(),
+    experimental: {
+      remoteFunctions: true,
+    },
+  },
+  compilerOptions: {
+    experimental: {
+      async: true,
+    },
+  },
+  vitePlugin: {
+    inspector: {
+      toggleKeyCombo: "alt-x",
+      showToggleButton: "always",
+      toggleButtonPos: "bottom-right",
+    },
+  },
+};
 
 export default config;
